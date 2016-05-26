5164fe4f711b8066429315f3ad82696c4d2462f9
LeaRulloda <lea_rulloda@dlsu.edu.ph>
2016-05-26 16:57:22 +0800

    Create act1.3.c

---
+++ act1.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	float efofex, x;
+	printf("Input value of x: ");
+	scanf("%f",&x);
+	efofex=x/70;
+	printf("The value of f(x) is: %.2f",efofex);
+	return 0;
+}
